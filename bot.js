9d0937776a466ba9556e536059b1df6b18f2b8e0
Aleksey <al.devel89@gmail.com>
2017-09-14 19:10:33 +0300

    fix bot js

--- bot.js
+++ bot.js
@@ -9,4 +9,6 @@ if (process.env.NODE_ENV === 'production') {
   bot = new TelegramBot(token, { polling: true })
 }
 
+console.log('Bot server started in the ' + process.env.NODE_ENV + ' mode')
+
 module.exports = bot
